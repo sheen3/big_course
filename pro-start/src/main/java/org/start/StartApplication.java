640715d0820e794d955b4579e914ebb77d83dd8a
sheen3 <sheen3@yeah.net>
2024-01-04 17:25:38 +0800

    Changes

--- pro-start/src/main/java/org/start/StartApplication.java
+++ pro-start/src/main/java/org/start/StartApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Author: eensh
  * @CreateDate: 2024/1/4
  */
-@SpringBootApplication(scanBasePackages = {"org.database", "org.user","org.tools","org.start"})
+@SpringBootApplication(scanBasePackages = {"org.database", "org.user","org.tools","org.start","org.product"})
 @SpringBootConfiguration
 public class StartApplication { public static void main(String[] args) {
     SpringApplication.run(StartApplication.class, args);
