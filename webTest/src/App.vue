7133129dc4660b30ec70fb446e85d4ab41fa0ad4
kokodayou2000 <1611601667@qq.com>
2024-01-04 23:48:34 +0800

    feat: add router Button

--- webTest/src/App.vue
+++ webTest/src/App.vue
@@ -1,8 +1,19 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import {useRouter} from "vue-router";
+const router = useRouter()
 </script>
 
 <template>
+  <div style="display: flex;">
+    <el-button @click="router.push('login')">登录</el-button>
+    <el-button @click="router.push('register')">注册</el-button>
+    <el-button @click="router.push('guanliyuan')">管理员</el-button>
+    <el-button @click="router.push('shengchanshang')">生产商</el-button>
+    <el-button @click="router.push('wuliu')">物流</el-button>
+    <el-button @click="router.push('zhijian')">质检</el-button>
+    <el-button @click="router.push('/')">首页</el-button>
+  </div>
   <RouterView />
 </template>
 
